f61f11c28e93a53334a18a808abf127f720aadb4
andymalv <andymalv@pm.me>
2025-12-20 11:52:26 -0500

    initial working streamlit app

--- Lab/plotting.py
+++ Lab/plotting.py
@@ -1,44 +1,92 @@
 # %%
-from typing import List
+from typing import List, Tuple
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import streamlit as st
 
 import biomechanics_analysis as biomech
 import gold_standard as gs
 
-# matplotlib.use("kitcat")
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-
-# import matplotlib
-
 
 # %%
-def load() -> (List[biomech.Session], List[gs.Session]):
+def load() -> List[biomech.Session]:
     file = "landmarks.csv"
     df = biomech.get_data(file)
     df = biomech.get_metrics(df)
 
-    gold = gs.get_data("joint_angles.csv")
+    # gold = gs.get_data("joint_angles.csv")
 
-    return df, gold
+    # return df, gold
+    return df
 
 
-# def plot():
 # %%
-df, gold = load()
-me = df[0].pitches[0].lead_knee
-them = gold[0].pitches[0].lead_knee
-x = range(np.shape(me)[0])
+def get_session(df: List[biomech.Session]) -> biomech.Session:
+    session_ids = []
+    for i in range(len(df)):
+        session_id = df[i].id
+        session_ids.append(session_id)
+    session_choice = st.selectbox("Select a session: ", session_ids)
+    session_index = session_ids.index(session_choice)
 
-fig, ax = plt.subplots()
-ax.plot(x, me.theta)
-ax.plot(x, them)
+    return df[session_index]
+
+
+def get_pitch(session: biomech.Session) -> biomech.Pitch:
+    pitch_num = len(session.pitches)
+    pitch_choice = st.selectbox("Select a pitch: ", range(1, pitch_num + 1))
+    pitch_index = int(pitch_choice) - 1
+
+    return session.pitches[pitch_index]
+
+
+def get_joint(pitch: biomech.Pitch) -> pd.DataFrame:
+    joint_list = ["Lead Knee"]
+    joint_choice = st.selectbox("Select a joint: ", joint_list)
+
+    match joint_choice:
+        case "Lead Knee":
+            joint_data = pitch.lead_knee
+
+    return joint_data
+
+
+def get_metric(joint: pd.DataFrame) -> Tuple[pd.Series, str]:
+    metric_list = ["Joint Angle"]
+    metric_choice = st.selectbox("Select a metric: ", metric_list)
+
+    match metric_choice:
+        case "Joint Angle":
+            metric_data = joint.theta
+            y_label = "Degrees"
+
+    return metric_data, y_label
 
 
 # %%
 def main():
-    pass
+    if "df" not in vars():
+        df = load()
+
+    session = get_session(df)
+    pitch = get_pitch(session)
+    joint = get_joint(pitch)
+    metric, y_label = get_metric(joint)
+
+    clicked = st.button("Load plot")
+
+    if clicked:
+        y = metric
+        x = range(len(y))
+
+        fig, ax = plt.subplots()
+        ax.plot(x, y)
+        ax.set(title="hold", ylabel=y_label)
+        st.pyplot(fig)
 
 
+# %%
 if __name__ == "__main__":
     main()
